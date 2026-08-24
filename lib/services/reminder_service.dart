d59729b247d5618270eaa9b657902893009f5644
ajeesh0905 <ajeesh0905@gmail.com>
2026-08-24 18:06:11 +0530

    Remove duplicated uiLocalNotificationDateInterpretation line

--- lib/services/reminder_service.dart
+++ lib/services/reminder_service.dart
@@ -124,7 +124,6 @@ class ReminderService {
         ),
       androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
       uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
-      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
       matchDateTimeComponents: recurringDaily ? DateTimeComponents.time : null,
       );
     final reminder = Reminder(
