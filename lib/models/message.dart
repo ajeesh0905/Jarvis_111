f4739db2590a4db7b376c43a45e0ea7b41495449
ajeesh0905 <ajeesh0905@gmail.com>
2026-08-25 07:16:33 +0530

    Add JSON serialization to ChatMessage for chat history persistence
    Added JSON serialization methods for ChatMessage.

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -19,4 +19,18 @@ class ChatMessage {
     final m = time.minute.toString().padLeft(2, '0');
     return '$h:$m';
   }
+
+  /// Serializes to JSON so chat history can be persisted in local phone
+  /// storage (SharedPreferences) and survive app restarts.
+  Map<String, dynamic> toJson() => {
+    'text': text,
+    'sender': sender.name,
+    'time': time.toIso8601String(),
+  };
+
+  factory ChatMessage.fromJson(Map<String, dynamic> json) => ChatMessage(
+    text: json['text'] as String,
+    sender: Sender.values.byName(json['sender'] as String),
+    time: DateTime.parse(json['time'] as String),
+  );
 }
